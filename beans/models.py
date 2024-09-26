7fe7898728782d47074144d3d18686ae2ce076ca
hwstar-1204 <115150798+hwstar-1204@users.noreply.github.com>
2024-09-26 17:59:34 +0900

    feat : DB 필드,테이블명 수정
    - Bean_Taste_Base : 원두 맛, 원두 맛&평가 테이블의 추상 모델 사용 (공통 필드가 많아서 추상화를 시키고 상속받게 하였다.)
    - 모든 테이블의 primary key는 {테이블명_id} 로 통일함

--- beans/models.py
+++ beans/models.py
@@ -23,43 +23,49 @@ class Bean(models.Model):
     bev_type = models.BooleanField(default=False, null=True, verbose_name="음료 유형")
     roastery = models.CharField(max_length=100, null=True, verbose_name="로스터리")
     variety = models.CharField(max_length=100, null=True, verbose_name="원두 품종")
-    is_user_created = models.DateTimeField(default=False, null=True, verbose_name="사용자 추가 여부")
+    is_user_created = models.BooleanField(default=False, null=True, verbose_name="사용자 추가 여부")  # TODO bool로 변경해야함
 
     def __str__(self):
-        return self.name
+        return f"{self.bean_id} - {self.name}"
 
     class Meta:
         db_table = 'bean'
         verbose_name = '원두'
         verbose_name_plural = '원두'
 
-class Bean_Taste(models.Model):
 
+class Bean_Taste_Base(models.Model):
     Taste_point_choices = list(zip(range(0, 6), range(0, 6)))  # 0~5
 
-    bean = models.OneToOneField(Bean, on_delete=models.CASCADE, verbose_name="원두")
     flavor = models.TextField(verbose_name="맛")
     body = models.IntegerField(choices=Taste_point_choices, verbose_name="바디감")
     acidity = models.IntegerField(choices=Taste_point_choices, verbose_name="산미")
     bitterness = models.IntegerField(choices=Taste_point_choices, verbose_name="쓴맛")
     sweetness = models.IntegerField(choices=Taste_point_choices, verbose_name="단맛")
 
+    class Meta:
+        abstract = True  # 추상 모델 사용
+
+class Bean_Taste(Bean_Taste_Base):
+    bean_taste_id = models.AutoField(primary_key=True)
+
     def __str__(self):
-        return self.bean.name
+        return f"{self.bean_taste_id} - {self.flavor}"
 
     class Meta:
         db_table = 'bean_taste'
         verbose_name = '원두 맛'
         verbose_name_plural = '원두 맛'
 
-class Bean_Taste_And_Review(Bean_Taste):
-    taste_review_id = models.OneToOneField('records.Tasted_Record', on_delete=models.CASCADE, verbose_name="시음기록")
-    star = models.IntegerField(choices=Bean_Taste.Taste_point_choices, verbose_name="별점")
+
+class Bean_Taste_Review(Bean_Taste_Base):
+    bean_taste_review_id = models.AutoField(primary_key=True)
+    star = models.IntegerField(choices=Bean_Taste_Base.Taste_point_choices, verbose_name="별점")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="작성일")
     place = models.CharField(max_length=100, verbose_name="시음 장소")
 
     def __str(self):
-        return f"bean :{self.taste_review_id.bean.name} - writer: {self.taste_review_id.user.nickname}"
+        return f"bean :{self.flavor} - {self.created_at}"
 
     class Meta:
         db_table = 'taste_and_review'
--- records/models.py
+++ records/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
-# TODO 시음기록 해야함!!
 class Tasted_Record(models.Model):
+    tasted_record_id = models.AutoField(primary_key=True)
     user = models.ForeignKey('profiles.User', on_delete=models.CASCADE, verbose_name="작성자")
     bean = models.ForeignKey('beans.Bean', on_delete=models.CASCADE, verbose_name="원두")
+    taste_and_review = models.OneToOneField('beans.Bean_Taste_Review', on_delete=models.CASCADE, verbose_name="맛&평가")
     content = models.TextField(verbose_name="노트 내용")
     view_cnt = models.IntegerField(default=0, verbose_name="조회수")
     like_cnt = models.IntegerField(default=0, verbose_name="좋아요 수")
@@ -20,9 +21,11 @@ class Tasted_Record(models.Model):
         verbose_name_plural = '시음 기록'
 
 class Post(models.Model):
+    post_id = models.AutoField(primary_key=True)
     user = models.ForeignKey('profiles.User', on_delete=models.CASCADE, verbose_name="작성자")
     title = models.CharField(max_length=200, verbose_name="제목")
     content = models.TextField(verbose_name="내용")
+    subject = models.CharField(max_length=100, verbose_name="주제")
     view_cnt = models.IntegerField(default=0, verbose_name="조회수")
     like_cnt = models.IntegerField(default=0, verbose_name="좋아요 수")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="작성일")
@@ -37,13 +40,18 @@ class Post(models.Model):
         verbose_name_plural = '게시글'
 
 class Photo(models.Model):
+    photo_id = models.AutoField(primary_key=True)
     post = models.ForeignKey(Post, on_delete=models.CASCADE, null=True, blank=True, verbose_name="관련 게시글")
     tasted_record = models.ForeignKey(Tasted_Record, on_delete=models.CASCADE, null=True, blank=True, verbose_name="관련 시음 기록")
     photo_url = models.URLField(max_length=500, verbose_name="사진 URL")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="업로드 일자")
 
     def __str__(self):
-        return f"Photo {self.id} - {self.photo_url}"
+        if self.post:
+            summery = f"Photo: {self.post.post_id}"
+        else:
+            summery = f"Photo: {self.tasted_record.tasted_record_id}"
+        return summery
 
     class Meta:
         db_table = 'photo'
