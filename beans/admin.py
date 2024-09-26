2cd36153207ce8bea921faf28903fd79d930061b
hwstar-1204 <115150798+hwstar-1204@users.noreply.github.com>
2024-09-26 17:55:50 +0900

    style : Admin 코드 추가
    생성된 모든 모델 admin에 추가함
    User
    Bean, Bean_Taste, Bean_Taste_Review
    Tasted_Record, Post, Photo

--- beans/admin.py
+++ beans/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Bean, Bean_Taste, Bean_Taste_Review
 
-# Register your models here.
+admin.site.register(Bean)
+admin.site.register(Bean_Taste)
+admin.site.register(Bean_Taste_Review)
--- records/admin.py
+++ records/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tasted_Record, Post, Photo
+
+admin.site.register(Tasted_Record)
+admin.site.register(Post)
+admin.site.register(Photo)
