11f70ad6db9c82c4dbf5d4faccafe32d33ac30b4
hwstar-1204 <hwstar1204@gmail.com>
2024-10-04 22:26:48 +0900

    test : 피드 팔로워들의 기록물 조회 테스트 코드
    ref commit 0e6a088b3de2078cce9f08c020a3db162e19de5f

---
+++ tests/feed_tests.py
@@ -0,0 +1,24 @@
+import pytest
+from django.urls import reverse
+from rest_framework import status
+from records.serializers import FeedSerializer
+import json
+
+
+@pytest.mark.django_db
+def test_feed_api_view(api_client, following_user, multiple_posts, multiple_tasted_records):
+    api_client.force_authenticate(user=following_user)
+    url = reverse('feed')
+    response = api_client.get(url, {'page': 1})
+
+    assert response.status_code == status.HTTP_200_OK
+    assert 'records' in response.data
+    assert 'has_next' in response.data
+
+    serialized_posts = FeedSerializer(multiple_posts + multiple_tasted_records, many=True).data
+    # print(json.dumps(response.data, indent=4))
+
+    response_data = sorted(response.data['records'], key=lambda x: x['created_at']) 
+    serializers_data = sorted(serialized_posts, key=lambda x: x['created_at'])
+    assert response_data == serializers_data
+    
