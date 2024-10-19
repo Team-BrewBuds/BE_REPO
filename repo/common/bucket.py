c5e1f0b28f405c037ff89c2b063dc95c9e7ac1e3
hwstar-1204 <hwstar1204@gmail.com>
2024-10-20 01:52:10 +0900

    style : migration 파일 생성
    이미지 필드 수정 및 마이그레이션 파일 merge

--- repo/common/bucket.py
+++ repo/common/bucket.py
@@ -1,5 +1,9 @@
 import uuid
+
+import boto3
+from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage, S3StaticStorage
+
 
 # 장고 MEDIA 파일을 다루는 각종 설정을 커스텀할 수 있습니다.
 #  - "media" 폴더에 저장되도록 location 설정을 해줍니다.
@@ -7,6 +11,7 @@ from storages.backends.s3boto3 import S3Boto3Storage, S3StaticStorage
 class AwsMediaStorage(S3Boto3Storage):
     location = "media"
     default_acl = "public-read"
+
 
 # 장고 STATIC 파일을 다루는 각종 설정을 커스텀할 수 있습니다.
 #  - "static" 폴더에 저장되도록 location 설정을 해줍니다.
@@ -25,28 +30,13 @@ def photo_upload_to(instance, filename):
     Returns:
         str: 저장 경로
     """
-    from repo.profiles.models import CustomUser
 
-    ext = filename.split('.')[-1]  # 파일 확장자
+    ext = filename.split(".")[-1]  # 파일 확장자
     unique_id = uuid.uuid4()
 
-    if isinstance(instance, CustomUser):
-        return f'profile/{unique_id}/.{ext}'
+    return f"others/{unique_id}.{ext}"
 
-    # Post 이미지
-    elif instance.post:
-        author_id = instance.post.author.id
-        post_id = instance.post.id
-        return f'post/{author_id}/{post_id}/{unique_id}.{ext}'
 
-    # TastedRecord 이미지
-    elif instance.tasted_record:
-        author_id = instance.tasted_record.author.id
-        tasted_record_id = instance.tasted_record.id
-        return f'tasted_record/{author_id}/{tasted_record_id}/{unique_id}.{ext}'
-
-    # 기본 경로 (이외 다른 경우)
-    return f'others/{uuid.uuid4()}.{ext}'
 
 def delete_photo_from_s3(photo_url: str) -> None:
     """
