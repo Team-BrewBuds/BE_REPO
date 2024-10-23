59534e01288b67117221bdc96ee9bbdb22efa826
hwstar-1204 <hwstar1204@gmail.com>
2024-10-23 16:19:58 +0900

    feat : 페이지 네이션 12개 default 설정

--- config/settings/_base.py
+++ config/settings/_base.py
@@ -69,7 +69,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": ("rest_framework_simplejwt.authentication.JWTAuthentication",),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 5,
+    "PAGE_SIZE": 12,
 }
 
 REST_AUTH = {
