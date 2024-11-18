93d5c8216f0efff3c0f4a849f897a24b9ca7882e
hwstar-1204 <hwstar1204@gmail.com>
2024-11-18 16:58:42 +0900

    style : tasted_record(시음기록) spectacular 문서 분리

--- repo/records/tasted_record/views.py
+++ repo/records/tasted_record/views.py
@@ -1,13 +1,11 @@
 from django.db import transaction
 from django.shortcuts import get_object_or_404
-from drf_spectacular.utils import OpenApiResponse, extend_schema, extend_schema_view
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from repo.common.permissions import IsOwnerOrReadOnly
-from repo.common.serializers import PageNumberSerializer
 from repo.common.utils import get_paginated_response_with_class
 from repo.common.view_counter import update_view_count
 from repo.records.models import TastedRecord
@@ -16,6 +14,7 @@ from repo.records.services import (
     get_tasted_record_detail,
     get_tasted_record_feed,
 )
+from repo.records.tasted_record.schemas import TastedRecordSchema
 from repo.records.tasted_record.serializers import (
     TastedRecordCreateUpdateSerializer,
     TastedRecordDetailSerializer,
@@ -27,42 +26,7 @@ from repo.records.tasted_record.service import (
 )
 
 
-@extend_schema_view(
-    get=extend_schema(
-        parameters=[PageNumberSerializer],
-        responses={200: TastedRecordListSerializer},
-        summary="홈 [시음기록] 피드 조회",
-        description="""
-            홈 피드의 시음기록 list를 최신순으로 가져옵니다.
-            - 순서: 팔로잉, 일반
-            - 정렬: 최신순
-            - 페이지네이션 적용 (12개)
-            - 30분이내 조회하지않은 게시글 가져옵니다.
-            - 프라이빗한 시음기록은 제외
-
-            Notice:
-            - like_cnt에서 likes로 변경
-            - comments(댓글 수), is_user_noted(사용자 저장여부) 추가 됨
-            - 비회원일경우 랜덤으로 시음기록을 가져옵니다.
-
-            담당자: hwstar1204
-        """,
-        tags=["tasted_records"],
-    ),
-    post=extend_schema(
-        request=TastedRecordCreateUpdateSerializer,
-        responses={
-            201: TastedRecordDetailSerializer,
-            400: OpenApiResponse(description="Bad Request (taste_review.star type: float)"),
-        },
-        summary="시음기록 생성",
-        description="""
-            단일 시음기록을 생성합니다.
-            담당자 : hwstar1204
-        """,
-        tags=["tasted_records"],
-    ),
-)
+@TastedRecordSchema.tasted_record_list_create_schema_view
 class TastedRecordListCreateAPIView(APIView):
     """
     홈 피드의 시음기록 list를 최신순으로 가져옵니다.
@@ -107,52 +71,7 @@ class TastedRecordListCreateAPIView(APIView):
             return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
 
-@extend_schema_view(
-    get=extend_schema(
-        responses={200: TastedRecordDetailSerializer},
-        summary="시음기록 상세 조회",
-        description="""
-            시음기록의 상세 정보를 가져옵니다.
-            담당자 : hwstar1204
-        """,
-        tags=["tasted_records"],
-    ),
-    put=extend_schema(
-        request=TastedRecordCreateUpdateSerializer,
-        responses={
-            200: TastedRecordDetailSerializer,
-            400: OpenApiResponse(description="Bad Request"),
-        },
-        summary="시음기록 수정",
-        description="""
-            시음기록의 정보를 수정합니다. (전체 수정)
-            담당자 : hwstar1204
-        """,
-        tags=["tasted_records"],
-    ),
-    patch=extend_schema(
-        request=TastedRecordCreateUpdateSerializer,
-        responses={
-            200: TastedRecordDetailSerializer,
-            400: OpenApiResponse(description="Bad Request"),
-        },
-        summary="시음기록 수정",
-        description="""
-            시음기록의 정보를 수정합니다. (일부 수정)
-            담당자 : hwstar1204
-        """,
-        tags=["tasted_records"],
-    ),
-    delete=extend_schema(
-        responses={204: OpenApiResponse(description="No Content")},
-        summary="시음기록 삭제",
-        description="""
-            시음기록을 삭제합니다.
-            담당자 : hwstar1204
-        """,
-        tags=["tasted_records"],
-    ),
-)
+@TastedRecordSchema.tasted_record_detail_schema_view
 class TastedRecordDetailApiView(APIView):
     """
     시음기록 상세정보 조회, 생성, 수정, 삭제 API
