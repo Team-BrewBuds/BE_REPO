bb7ffa30db68af11de0293a966cf7b44f4233f65
hwstar-1204 <hwstar1204@gmail.com>
2024-11-21 01:08:53 +0900

    style : spectacular 문서 버전 업데이트
    참고 문서 링크 추가

--- config/settings/settings_modules/spectacular.py
+++ config/settings/settings_modules/spectacular.py
@@ -1,7 +1,9 @@
 # drf-spectacular
 SPECTACULAR_SETTINGS = {
     "TITLE": "brewbuds API",
-    "DESCRIPTION": "developing API",
-    "VERSION": "1.0.0",
+    "DESCRIPTION": "API 변경 내역은 "
+    + "[여기](https://www.notion.so/battingeye/05-API-2189da6e59eb43e9b39151dcebc82026?pvs=4)"
+    + "에서 확인해주세요.",
+    "VERSION": "v0.0.6",
     "SERVE_INCLUDE_SCHEMA": False,
 }
