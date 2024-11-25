c8b8b15ce19ccab7adb962dfd22d57a4a9811e46
hwstar-1204 <hwstar1204@gmail.com>
2024-11-26 02:54:12 +0900

    style : relationship 모델 메니저 삭제

--- repo/interactions/models.py
+++ repo/interactions/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-from repo.interactions.managers import RelationshipManager
 from repo.profiles.models import CustomUser
 
 
@@ -15,8 +14,6 @@ class Relationship(models.Model):
     to_user = models.ForeignKey(CustomUser, related_name="relationships_to", on_delete=models.CASCADE)
     relationship_type = models.CharField(max_length=10, choices=RELATIONSHIP_TYPE_CHOICES, verbose_name="관계 유형")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="생성일")
-
-    objects = RelationshipManager()
 
     def __str__(self):
         return f"{self.from_user.nickname} {self.get_relationship_type_display()} {self.to_user.nickname}"
