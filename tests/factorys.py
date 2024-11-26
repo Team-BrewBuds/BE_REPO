295246fb1700596e593ba53abb3728a7208771ca
hwstar-1204 <hwstar1204@gmail.com>
2024-11-26 21:56:23 +0900

    test : 노트 API 테스트 수정
    - url endpoint 변경
    - 에외 처리 수정
    - 모델 import 경로 수정

--- tests/factorys.py
+++ tests/factorys.py
@@ -5,9 +5,10 @@ from factory.django import DjangoModelFactory
 from faker import Faker
 
 from repo.beans.models import Bean, BeanTasteReview
+from repo.interactions.note.models import Note
 from repo.interactions.relationship.models import Relationship
 from repo.profiles.models import CustomUser, UserDetail
-from repo.records.models import Comment, Note, Photo, Post, Report, TastedRecord
+from repo.records.models import Comment, Photo, Post, Report, TastedRecord
 
 fake = Faker("ko_KR")
 
