e0b24f022e9fbb50523d36b1cf6504f022a4f21a
hwstar-1204 <hwstar1204@gmail.com>
2024-12-03 03:04:46 +0900

    style : 불필요한 매서드 삭제

--- repo/records/services.py
+++ repo/records/services.py
@@ -1,20 +1,9 @@
 import random
 from itertools import chain
 
-from django.db.models import (
-    BooleanField,
-    Count,
-    Exists,
-    Prefetch,
-    Q,
-    Value,
-)
-
 from repo.common.view_counter import get_not_viewed_contents
-from repo.interactions.like.services import LikeService
-from repo.interactions.note.services import NoteService
 from repo.interactions.relationship.services import RelationshipService
-from repo.records.models import Photo, Post, TastedRecord
+from repo.records.models import Post, TastedRecord
 from repo.records.posts.serializers import PostListSerializer
 from repo.records.posts.services import get_post_service
 from repo.records.tasted_record.serializers import TastedRecordListSerializer
@@ -47,84 +36,10 @@ def get_serialized_data(request, page_obj_list):
         if isinstance(item, TastedRecord):
             serializer_data = TastedRecordListSerializer(item, context={"request": request}).data
             obj_list.append(serializer_data)
-            # obj_list.append(serialize_tasted_record_list(item, request))
-        else:
+        elif isinstance(item, Post):
             serializer_data = PostListSerializer(item, context={"request": request}).data
             obj_list.append(serializer_data)
-            # obj_list.append(serialize_post_list(item, request))
     return obj_list
-
-
-def get_post_feed_queryset(user, add_filter=None, exclude_filter=None, subject=None):  # TODO: post service로 이동 V
-    """
-    게시글 피드를 위한 필터링된 쿼리셋을 생성합니다.
-
-    Args:
-        user: 사용자 객체
-        add_filter: 추가할 필터 조건 (dict)
-        exclude_filter: 제외할 필터 조건 (dict)
-        subject: 게시글 주제
-
-    Returns:
-        QuerySet: 필터링된 게시글 쿼리셋
-    """
-    like_service = LikeService("post")
-    note_service = NoteService()
-
-    add_filters = Q(**add_filter) if add_filter else Q()
-    add_filters &= Q(subject=subject) if subject is not None else Q()
-
-    exclude_filters = Q(**exclude_filter) if exclude_filter else Q()
-
-    return (
-        Post.objects.filter(add_filters)
-        .exclude(exclude_filters)
-        .select_related("author")
-        .prefetch_related("tasted_records", "comment_set", "note_set", Prefetch("photo_set", queryset=Photo.objects.only("photo_url")))
-        .annotate(
-            likes=Count("like_cnt", distinct=True),
-            comments=Count("comment", distinct=True),
-            is_user_liked=Exists(like_service.get_like_subquery_for_post(user)),
-            is_user_noted=Exists(note_service.get_note_subquery_for_post(user)),
-        )
-    )
-
-
-def get_tasted_record_feed_queryset(user, add_filter=None, exclude_filter=None):  # TODO: tasted record service로 이동
-    """
-    시음기록 피드를 위한 필터링된 쿼리셋을 생성합니다.
-
-    Args:
-        user: 사용자 객체
-        add_filter: 추가할 필터 조건 (dict)
-        exclude_filter: 제외할 필터 조건 (dict)
-
-    Returns:
-        QuerySet: 필터링된 시음기록 쿼리셋
-    """
-    like_service = LikeService("tasted_record")
-    note_service = NoteService()
-
-    add_filters = Q(**add_filter) if add_filter else Q()
-    exclude_filters = Q(**exclude_filter) if exclude_filter else Q()
-
-    return (
-        TastedRecord.objects.filter(add_filters)
-        .exclude(exclude_filters)
-        .select_related("author", "bean", "taste_review")
-        .prefetch_related("comment_set", "note_set", Prefetch("photo_set", queryset=Photo.objects.only("photo_url")))
-        .annotate(
-            likes=Count("like_cnt", distinct=True),
-            comments=Count("comment", distinct=True),
-            is_user_liked=Exists(like_service.get_like_subquery_for_tasted_record(user)),
-            is_user_noted=Exists(note_service.get_note_subquery_for_tasted_record(user)),
-        )
-    )
-
-
-################################ 피드 관련 매서드 ################################
-################################ 피드 관련 매서드 ################################
-################################ 피드 관련 매서드 ################################
 
 
 def get_following_feed(request, user):
@@ -217,55 +132,6 @@ def get_refresh_feed(user):
     return combined_data
 
 
-def get_annonymous_tasted_records_feed():  # TODO: tasted record service로 이동
-    """
-    비로그인 사용자를 위한 시음기록 피드를 반환합니다.
-
-    - 공개된 시음기록만 포함
-    - 좋아요와 저장 상태는 항상 False
-    - 랜덤 순서로 정렬
-
-    Returns:
-        QuerySet: 랜덤으로 정렬된 시음기록 쿼리셋
-    """
-    return (
-        TastedRecord.objects.filter(is_private=False)
-        .select_related("author", "bean", "taste_review")
-        .prefetch_related("comment_set", "note_set", Prefetch("photo_set", queryset=Photo.objects.only("photo_url")))
-        .annotate(
-            likes=Count("like_cnt", distinct=True),
-            comments=Count("comment", distinct=True),
-            is_user_liked=Value(False, output_field=BooleanField()),
-            is_user_noted=Value(False, output_field=BooleanField()),
-        )
-        .order_by("?")
-    )
-
-
-def get_annonymous_posts_feed():  # TODO: post service로 이동
-    """
-    비로그인 사용자를 위한 게시글 피드를 반환합니다.
-
-    - 모든 게시글 포함
-    - 좋아요와 저장 상태는 항상 False
-    - 랜덤 순서로 정렬
-
-    Returns:
-        QuerySet: 랜덤으로 정렬된 게시글 쿼리셋
-    """
-    return (
-        Post.objects.select_related("author")
-        .prefetch_related("tasted_records", "comment_set", "note_set", Prefetch("photo_set", queryset=Photo.objects.only("photo_url")))
-        .annotate(
-            likes=Count("like_cnt", distinct=True),
-            comments=Count("comment", distinct=True),
-            is_user_liked=Value(False, output_field=BooleanField()),
-            is_user_noted=Value(False, output_field=BooleanField()),
-        )
-        .order_by("?")
-    )
-
-
 def annonymous_user_feed():
     """
     비로그인 사용자를 위한 통합 피드를 반환합니다.
@@ -283,8 +149,3 @@ def annonymous_user_feed():
     random.shuffle(combined_data)
 
     return combined_data
-
-
-################################ 피드 관련 매서드 ################################
-################################ 피드 관련 매서드 ################################
-################################ 피드 관련 매서드 ################################
