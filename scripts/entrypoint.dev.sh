ed6547c9468ee105660d30337a2b557706b4bb94
hwstar-1204 <hwstar1204@gmail.com>
2024-12-18 20:23:59 +0900

    feat : dev docker-compose 스크립트 파일 설정
    django 서버 실행 및 celery 실행 명령을 스크립트 파일에서 관리

---
+++ scripts/entrypoint.dev.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# exit on error
+set -e
+
+# Apply database migrations
+echo "Applying database migrations"
+python manage.py migrate
+
+# Collect static files
+#echo "Collecting static files"
+#python manage.py collectstatic --noinput
+
+# Start the Dev server
+echo "Starting dev server"
+gunicorn config.wsgi.dev:application --bind 0.0.0.0:8000 &
+
+# start celery worker
+echo "Starting celery worker"
+celery -A config worker -l info &
+
+# start celery beat
+echo "Starting celery beat"
+celery -A config beat -l info &
+
+# Wait for all background processes to complete
+# print background jobs
+jobs -l
+wait
