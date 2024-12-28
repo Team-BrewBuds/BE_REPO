089de238550383c3b6c3efae955e42a0bd69c248
hwstar-1204 <hwstar1204@gmail.com>
2024-12-28 18:07:07 +0900

    feat : 신고 관련 Admin 기능 추가

--- repo/interactions/admin.py
+++ repo/interactions/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 
 from repo.interactions.note.models import Note
 from repo.interactions.relationship.models import Relationship
+from repo.interactions.report.models import Report
+
+
+class ReportAdmin(admin.ModelAdmin):
+    list_display = ["author", "object_type", "reason", "object_id", "status", "created_at"]
+    list_filter = ["status", "object_type", "reason", "created_at"]
+    search_fields = ["author__username", "object_type", "object_id"]
+    ordering = ["id"]
+
 
 admin.site.register(Relationship)
 admin.site.register(Note)
+admin.site.register(Report, ReportAdmin)
