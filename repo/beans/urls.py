ccb19b86c7eaa5881ae23c8af6f43c2e267148f6
Blake Jeong <jmchan2432@gmail.com>
2024-12-30 23:59:51 +0900

    feat: 원두 정보 상세 추가 1차

--- repo/beans/urls.py
+++ repo/beans/urls.py
@@ -5,4 +5,5 @@ from repo.beans.views import *
 urlpatterns = [
     path("search/", BeanNameSearchView.as_view(), name="bean_search"),
     path("profile/<int:id>/", UserBeanListAPIView.as_view(), name="user_beans"),
+    path("detail/<int:id>/", BeanDetailView.as_view(), name="bean_detail"),
 ]
