ba72c432a40904581d69ffd78ba05f2b69d4eba5
Blake Jeong <jmchan2432@gmail.com>
2025-01-12 22:23:13 +0900

    feat: 공식원두 모델 추가

--- repo/beans/models.py
+++ repo/beans/models.py
@@ -21,6 +21,7 @@ class Bean(models.Model):
     roastery = models.CharField(max_length=100, null=True, blank=True, verbose_name="로스터리")
     variety = models.CharField(max_length=100, null=True, blank=True, verbose_name="원두 품종")
     is_user_created = models.BooleanField(default=False, null=True, blank=True, verbose_name="사용자 추가 여부")
+    is_official = models.BooleanField(default=False, null=True, blank=True, verbose_name="공식원두 여부")
 
     def __str__(self):
         return f"{self.id} - {self.name}"
@@ -29,6 +30,42 @@ class Bean(models.Model):
         db_table = "bean"
         verbose_name = "원두"
         verbose_name_plural = "원두"
+
+
+class OfficialBean(models.Model):
+
+    bean_type_choices = [("single", "싱글 오리진"), ("blend", "블랜드")]
+    roasting_point_choices = list(zip(range(0, 6), range(0, 6)))  # 0~5
+
+    # 원두 맛
+    bean_taste = models.OneToOneField(
+        "BeanTaste", on_delete=models.CASCADE, null=True, blank=True, related_name="official_bean", verbose_name="원두 맛"
+    )
+
+    # 원두 공식 사진
+    image_url = models.ImageField(upload_to="beans/images/", null=True, blank=True, verbose_name="공식원두 이미지")
+
+    bean_type = models.CharField(max_length=20, choices=bean_type_choices, verbose_name="원두 유형")
+    is_decaf = models.BooleanField(default=False, verbose_name="디카페인")
+    name = models.CharField(max_length=100, verbose_name="원두명")
+    origin_country = models.CharField(max_length=100, verbose_name="원산지")
+
+    extraction = models.CharField(max_length=50, null=True, blank=True, verbose_name="추출 방식")
+    roast_point = models.IntegerField(choices=roasting_point_choices, null=True, blank=True, verbose_name="로스팅 포인트")
+    process = models.CharField(max_length=50, null=True, blank=True, verbose_name="가공 방식")
+    region = models.CharField(max_length=100, null=True, blank=True, verbose_name="지역")
+    bev_type = models.BooleanField(default=False, null=True, blank=True, verbose_name="음료 유형")
+    roastery = models.CharField(max_length=100, null=True, blank=True, verbose_name="로스터리")
+    variety = models.CharField(max_length=100, null=True, blank=True, verbose_name="원두 품종")
+    is_user_created = models.BooleanField(default=False, null=True, blank=True, verbose_name="사용자 추가 여부")
+
+    def __str__(self):
+        return f"{self.id} - {self.name}"
+
+    class Meta:
+        db_table = "official_bean"
+        verbose_name = "공식원두"
+        verbose_name_plural = "공식원두"
 
 
 class BeanTasteBase(models.Model):
