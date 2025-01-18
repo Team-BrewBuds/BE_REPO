fbab1d308c9c88ecb9a546719b11627ba02c16bc
hwstar-1204 <hwstar1204@gmail.com>
2025-01-18 14:13:56 +0900

    refact : 공식/비공식 원두 모델 통합
    - 공식 원두, 비공식 원두 -> Bean 모델로 통합
    - 원두 데이터를 수정할 수 없기 때문에 통합하여 관리 가능
    - 공식 원두이면 무조건 bean_taste에 기본 맛을 가지고 있어야함  (공식원두 생성시 참조하도록 설정 필요)
    - 공식/비공식 원두 모두 bean_taste_review를 참조 가능
    - 공식 원두 이미지는 static 경로 설정 필요

--- repo/beans/models.py
+++ repo/beans/models.py
@@ -2,17 +2,17 @@ from django.db import models
 
 
 class Bean(models.Model):
-
     bean_type_choices = [("single", "싱글 오리진"), ("blend", "블랜드")]
     roasting_point_choices = list(zip(range(0, 6), range(0, 6)))  # 0~5
 
     # 필수
     bean_type = models.CharField(max_length=20, choices=bean_type_choices, verbose_name="원두 유형")
-    is_decaf = models.BooleanField(default=False, verbose_name="디카페인")
     name = models.CharField(max_length=100, verbose_name="원두명")
     origin_country = models.CharField(max_length=100, verbose_name="원산지")
+    image_url = models.URLField(null=True, blank=True, verbose_name="공식원두 이미지")
 
     # 선택
+    is_decaf = models.BooleanField(default=False, verbose_name="디카페인")
     extraction = models.CharField(max_length=50, null=True, blank=True, verbose_name="추출 방식")
     roast_point = models.IntegerField(choices=roasting_point_choices, null=True, blank=True, verbose_name="로스팅 포인트")
     process = models.CharField(max_length=50, null=True, blank=True, verbose_name="가공 방식")
@@ -32,42 +32,6 @@ class Bean(models.Model):
         verbose_name_plural = "원두"
 
 
-class OfficialBean(models.Model):
-
-    bean_type_choices = [("single", "싱글 오리진"), ("blend", "블랜드")]
-    roasting_point_choices = list(zip(range(0, 6), range(0, 6)))  # 0~5
-
-    # 원두 맛
-    bean_taste = models.OneToOneField(
-        "BeanTaste", on_delete=models.CASCADE, null=True, blank=True, related_name="official_bean", verbose_name="원두 맛"
-    )
-
-    # 원두 공식 사진
-    image_url = models.ImageField(upload_to="beans/images/", null=True, blank=True, verbose_name="공식원두 이미지")
-
-    bean_type = models.CharField(max_length=20, choices=bean_type_choices, verbose_name="원두 유형")
-    is_decaf = models.BooleanField(default=False, verbose_name="디카페인")
-    name = models.CharField(max_length=100, verbose_name="원두명")
-    origin_country = models.CharField(max_length=100, verbose_name="원산지")
-
-    extraction = models.CharField(max_length=50, null=True, blank=True, verbose_name="추출 방식")
-    roast_point = models.IntegerField(choices=roasting_point_choices, null=True, blank=True, verbose_name="로스팅 포인트")
-    process = models.CharField(max_length=50, null=True, blank=True, verbose_name="가공 방식")
-    region = models.CharField(max_length=100, null=True, blank=True, verbose_name="지역")
-    bev_type = models.BooleanField(default=False, null=True, blank=True, verbose_name="음료 유형")
-    roastery = models.CharField(max_length=100, null=True, blank=True, verbose_name="로스터리")
-    variety = models.CharField(max_length=100, null=True, blank=True, verbose_name="원두 품종")
-    is_user_created = models.BooleanField(default=False, null=True, blank=True, verbose_name="사용자 추가 여부")
-
-    def __str__(self):
-        return f"{self.id} - {self.name}"
-
-    class Meta:
-        db_table = "official_bean"
-        verbose_name = "공식원두"
-        verbose_name_plural = "공식원두"
-
-
 class BeanTasteBase(models.Model):
     Taste_point_choices = list(zip(range(0, 6), range(0, 6)))  # 0~5
 
@@ -82,15 +46,17 @@ class BeanTasteBase(models.Model):
 
 
 class BeanTaste(BeanTasteBase):
-    id = models.AutoField(primary_key=True)
+    bean = models.ForeignKey(
+        "Bean", on_delete=models.CASCADE, null=True, blank=True, related_name="bean_taste", verbose_name="공식 원두 기본 맛"
+    )
 
     def __str__(self):
         return f"{self.id} - {self.flavor}"
 
     class Meta:
         db_table = "bean_taste"
-        verbose_name = "원두 맛"
-        verbose_name_plural = "원두 맛"
+        verbose_name = "공식 원두 기본 맛"
+        verbose_name_plural = "공식 원두 기본 맛"
 
 
 class BeanTasteReview(BeanTasteBase):
