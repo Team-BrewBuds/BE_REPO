6131784122319c3e5966a9de84f8bc4def7f2a61
hwstar-1204 <hwstar1204@gmail.com>
2025-02-04 02:14:46 +0900

    feat : admin report 유저 활동 개수 데이터 추가

--- repo/interactions/report/admin_services.py
+++ repo/interactions/report/admin_services.py
@@ -1,4 +1,4 @@
-from django.db.models import Min, OuterRef, Q, Subquery
+from django.db.models import Count, Min, OuterRef, Q, Subquery
 
 from repo.common.utils import make_date_format
 from repo.interactions.note.models import Note
@@ -11,6 +11,11 @@ def get_users_activity_report() -> list[dict]:
     사용자 활동 보고서를 생성하는 함수
     """
     user_activities = CustomUser.objects.annotate(
+        tr_count=Count("tastedrecord"),
+        post_count=Count("post"),
+        noted_bean_count=Count("note__bean"),
+        noted_tr_count=Count("note__tasted_record"),
+        noted_post_count=Count("note__post"),
         first_tr_at=Min("tastedrecord__created_at"),
         first_post_at=Min("post__created_at"),
         second_tr_at=get_second_record_subquery(TastedRecord),
@@ -22,6 +27,11 @@ def get_users_activity_report() -> list[dict]:
         "id",
         "nickname",
         "created_at",
+        "tr_count",
+        "post_count",
+        "noted_bean_count",
+        "noted_tr_count",
+        "noted_post_count",
         "first_tr_at",
         "first_post_at",
         "second_tr_at",
@@ -36,6 +46,11 @@ def get_users_activity_report() -> list[dict]:
             "ID": user_activity["id"],
             "닉네임": user_activity["nickname"],
             "가입일": make_date_format(user_activity["created_at"]),
+            "시음기록 작성 개수": user_activity["tr_count"],
+            "게시물 작성 개수": user_activity["post_count"],
+            "원두정보 저장 개수": user_activity["noted_bean_count"],
+            "시음기록 저장 개수": user_activity["noted_tr_count"],
+            "게시물 저장 개수": user_activity["noted_post_count"],
             "첫 시음기록 작성일": make_date_format(user_activity["first_tr_at"]),
             "두번째 시음기록 작성일": make_date_format(user_activity["second_tr_at"]),
             "첫 게시물 작성일": make_date_format(user_activity["first_post_at"]),
