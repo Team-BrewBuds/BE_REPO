d7e271ffafeaa20250b9558ae974317e7a3bd85e
Blake Jeong <jmchan2432@gmail.com>
2025-03-04 18:40:44 +0900

    feat: 추천 구조 수정

--- repo/recommendation/apps.py
+++ repo/recommendation/apps.py
@@ -1,6 +1,13 @@
 from django.apps import AppConfig
 
+from repo.recommendation.loaders import ModelLoader
+
 
 class RecommendationConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "repo.recommendation"
+
+    def ready(self):
+        """서버가 시작될 때 머신러닝 모델과 인코더를 미리 로드"""
+
+        ModelLoader.load()
