aef2b6b91b101135d02d171a5469036e64e18bae
hwstar-1204 <hwstar1204@gmail.com>
2025-05-03 16:29:30 +0900

    feat : 댓글 삭제 커스텀 권한 설정

--- repo/records/comment/views.py
+++ repo/records/comment/views.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from repo.common.permissions import IsOwnerOrReadOnly
+from repo.common.permissions import IsAuthorOrOwner, IsOwnerOrReadOnly
 from repo.common.utils import get_paginated_response_with_class
 
 from .schemas import CommentDetailSchema, CommentSchema
@@ -53,20 +53,23 @@ class CommentDetailAPIView(APIView):
     담당자: hwstar1204
     """
 
-    permission_classes = [IsOwnerOrReadOnly]
+    def get_permissions(self):
+        return [IsAuthorOrOwner()] if self.request.method == "DELETE" else [IsOwnerOrReadOnly()]
 
     def __init__(self, **kwargs):
+        super().__init__(**kwargs)
         self.comment_service = CommentService()
 
-    def get_object(self, pk):
-        comment = self.comment_service.get_comment_by_id(pk)
+    def get_object(self, id):
+        comment = self.comment_service.get_comment_by_id(id)
         self.check_object_permissions(self.request, comment)
         return comment
 
     def get(self, request, id):
-        comment = self.comment_service.get_comment_detail(self.get_object(id))
+        comment = self.get_object(id)
+        comment_detail = self.comment_service.get_comment_detail(comment)
 
-        response_serializer = CommentOutputSerializer(comment, context={"request": request})
+        response_serializer = CommentOutputSerializer(comment_detail, context={"request": request})
         return Response(response_serializer.data, status=status.HTTP_200_OK)
 
     def patch(self, request, id):
@@ -74,12 +77,13 @@ class CommentDetailAPIView(APIView):
         valid_serializer.is_valid(raise_exception=True)
 
         comment = self.get_object(id)
-        comment = self.comment_service.update_comment(comment, valid_serializer.validated_data)
+        updated_comment = self.comment_service.update_comment(comment, valid_serializer.validated_data)
 
-        response_serializer = CommentOutputSerializer(comment, context={"request": request})
+        response_serializer = CommentOutputSerializer(updated_comment, context={"request": request})
         return Response(response_serializer.data, status=status.HTTP_200_OK)
 
     def delete(self, request, id):
         comment = self.get_object(id)
         self.comment_service.delete_comment(comment)
+
         return Response(status=status.HTTP_204_NO_CONTENT)
