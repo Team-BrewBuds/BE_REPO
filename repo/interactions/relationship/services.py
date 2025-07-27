2689abe1a1cd2242cab4abe1f6c610b0ab43a53d
hwstar-1204 <hwstar1204@gmail.com>
2025-07-27 16:51:35 +0900

    refact : relationship service에서 manager 쿼리셋 재사용하도록 수정
    - 상수 타입 사용으로 통일
    - 확장성을 위해 int 타입 -> user 객체를 사용하도록 수정
    - 불필요 코드 삭제
    - type hint, doc string 추가

--- repo/interactions/relationship/services.py
+++ repo/interactions/relationship/services.py
@@ -1,5 +1,5 @@
 from django.db import models, transaction
-from django.db.models import Case, Exists, OuterRef, Q, When
+from django.db.models import Exists
 
 from repo.common.exception.exceptions import (
     BadRequestException,
@@ -7,108 +7,101 @@ from repo.common.exception.exceptions import (
     ForbiddenException,
     NotFoundException,
 )
+from repo.profiles.models import CustomUser
 
+from .enums import RelationshipType
 from .models import Relationship
-
-FOLLOW_TYPE = "follow"
-BLOCK_TYPE = "block"
 
 
 class RelationshipService:
     """관계 관련 비즈니스 로직을 처리하는 서비스"""
 
-    def check_relationship(self, from_user, to_user, relationship_type):
-        return Relationship.objects.filter(from_user=from_user, to_user=to_user, relationship_type=relationship_type).exists()
-
-    def double_check_relationships(self, from_user, to_user, relationship_type):
-        return Relationship.objects.filter(
-            Q(from_user=from_user, to_user=to_user, relationship_type=relationship_type)
-            | Q(from_user=to_user, to_user=from_user, relationship_type=relationship_type)
-        ).exists()
-
-    def follow(self, from_user, to_user):
-        if self.double_check_relationships(from_user, to_user, BLOCK_TYPE):
-            raise ForbiddenException("user is blocking or blocked")
-
-        relationship, created = Relationship.objects.get_or_create(from_user=from_user, to_user=to_user, relationship_type=FOLLOW_TYPE)
-
-        if not created:
+    def follow(self, from_user: CustomUser, to_user: CustomUser) -> Relationship:
+        """팔로우 관계 생성"""
+        if Relationship.objects.one_way_check(from_user, to_user, RelationshipType.FOLLOW.name):
             raise ConflictException("user is already following")
 
-    def unfollow(self, from_user, to_user):
-        if not self.check_relationship(from_user, to_user, FOLLOW_TYPE):
+        if Relationship.objects.two_way_check(from_user, to_user, RelationshipType.BLOCK.name, operator="or"):
+            raise ForbiddenException("user is blocking or blocked")
+
+        return Relationship.objects.create(from_user=from_user, to_user=to_user, relationship_type=RelationshipType.FOLLOW.name)
+
+    def unfollow(self, from_user: CustomUser, to_user: CustomUser) -> None:
+        """팔로우 관계 삭제"""
+        if not Relationship.objects.one_way_check(from_user, to_user, RelationshipType.FOLLOW.name):
             raise NotFoundException("user is not following")
 
-        relationship = Relationship.objects.filter(from_user=from_user, to_user=to_user, relationship_type=FOLLOW_TYPE)
+        relationship = Relationship.objects.filter(from_user=from_user, to_user=to_user, relationship_type=RelationshipType.FOLLOW.name)
         relationship.delete()
 
     @transaction.atomic
-    def block(self, from_user, to_user):
-        try:
-            self.unfollow(from_user, to_user)
-        except NotFoundException:
-            pass
+    def block(self, from_user: CustomUser, to_user: CustomUser) -> Relationship:
+        """차단 관계 생성"""
+        if from_user == to_user:
+            raise BadRequestException("user cannot block itself")
 
-        relationship, created = Relationship.objects.get_or_create(from_user=from_user, to_user=to_user, relationship_type=BLOCK_TYPE)
-
-        if not created:
+        if Relationship.objects.one_way_check(from_user, to_user, RelationshipType.BLOCK.name):
             raise ConflictException("user is already blocking")
 
+        if Relationship.objects.one_way_check(from_user, to_user, RelationshipType.FOLLOW.name):
+            self.unfollow(from_user, to_user)
+
+        return Relationship.objects.create(from_user=from_user, to_user=to_user, relationship_type=RelationshipType.BLOCK.name)
+
     @transaction.atomic
-    def unblock(self, from_user, to_user):
-        if not self.check_relationship(from_user, to_user, BLOCK_TYPE):
+    def unblock(self, from_user: CustomUser, to_user: CustomUser) -> None:
+        """차단 관계 삭제"""
+        if not Relationship.objects.one_way_check(from_user, to_user, RelationshipType.BLOCK.name):
             raise NotFoundException("user is not blocking")
 
-        relationship = Relationship.objects.filter(from_user=from_user, to_user=to_user, relationship_type=BLOCK_TYPE)
+        relationship = Relationship.objects.filter(from_user=from_user, to_user=to_user, relationship_type=RelationshipType.BLOCK.name)
         relationship.delete()
 
-    def get_following(self, user_id):
-        return Relationship.objects.filter(from_user=user_id, relationship_type=FOLLOW_TYPE)
+    def get_following(self, user: CustomUser) -> models.QuerySet:
+        """특정 유저가 팔로잉하는 유저 필터링"""
+        return Relationship.objects.get_following(user)
 
-    def get_followers(self, user_id):
-        return Relationship.objects.filter(to_user=user_id, relationship_type=FOLLOW_TYPE)
+    def get_followers(self, user: CustomUser) -> models.QuerySet:
+        """특정 유저를 팔로잉하는 유저 필터링"""
+        return Relationship.objects.get_followers(user)
 
-    def get_following_user_list(self, user_id):
-        return self.get_following(user_id).values_list("to_user", flat=True)
+    def get_following_user_list(self, user: CustomUser) -> list[int]:
+        """특정 유저가 팔로잉하는 유저 필터링"""
+        return self.get_following(user).values_list("to_user", flat=True)
 
-    def get_followers_user_list(self, user_id):
-        return self.get_followers(user_id).values_list("from_user", flat=True)
+    def get_followers_user_list(self, user: CustomUser) -> list[int]:
+        """특정 유저를 팔로잉하는 유저 필터링"""
+        return self.get_followers(user).values_list("from_user", flat=True)
 
-    def get_blocking(self, user_id):
-        return Relationship.objects.filter(from_user=user_id, relationship_type=BLOCK_TYPE)
+    def get_blocking(self, user: CustomUser) -> models.QuerySet:
+        """특정 유저가 차단한 유저 필터링"""
+        return Relationship.objects.get_blocking(user)
 
-    def get_blocked(self, user_id):
-        return Relationship.objects.filter(to_user=user_id, relationship_type=BLOCK_TYPE)
+    def get_blocked(self, user: CustomUser) -> models.QuerySet:
+        """특정 유저가 차단당한 유저 필터링"""
+        return Relationship.objects.get_blocked(user)
 
-    def get_unique_blocked_user_list(self, user_id: int):
-        """특정 유저와 차단 관계에 있는 유저 목록을 조회하는 메서드"""
-        return (
-            Relationship.objects.filter(Q(from_user=user_id) | Q(to_user=user_id), relationship_type=BLOCK_TYPE)
-            .values("from_user", "to_user")
-            .values_list(
-                Case(
-                    When(from_user=user_id, then="to_user"),
-                    When(to_user=user_id, then="from_user"),
-                ),
-                flat=True,
-            )
-            .distinct()
-        )
+    def get_unique_blocked_user_list(self, user: CustomUser) -> list[int]:
+        """특정 유저와 차단 관계에 있는 유저 목록을 조회"""
+        return Relationship.objects.get_unique_blocked_user_list(user)
 
-    def get_user_relationships_by_follow_type(self, follow_type, request_user, target_user=None):
+    def get_user_relationships_by_follow_type(
+        self, follow_type: str, request_user: CustomUser, target_user: CustomUser | None = None
+    ) -> list[dict]:
+        """특정 유저의 팔로잉 또는 팔로워 관계 조회"""
         if target_user is None:
             target_user = request_user
 
         base_query = {
             "following": {
-                "base": self.get_following(target_user.id),
+                "base": self.get_following(target_user),
                 "select_related_field": "to_user",
                 "user_field": "to_user",
                 "is_following_from_user": request_user,
                 "is_following_to_user": models.OuterRef("to_user_id"),
             },
             "follower": {
-                "base": self.get_followers(target_user.id),
+                "base": self.get_followers(target_user),
                 "select_related_field": "from_user",
                 "user_field": "from_user",
                 "is_following_from_user": request_user,
@@ -129,7 +122,7 @@ class RelationshipService:
                     Relationship.objects.filter(
                         from_user=query_config["is_following_from_user"],
                         to_user_id=query_config["is_following_to_user"],
-                        relationship_type=FOLLOW_TYPE,
+                        relationship_type=RelationshipType.FOLLOW.name,
                     )
                 )
             )
@@ -141,5 +134,6 @@ class RelationshipService:
             for relationship in relationships
         ]
 
-    def get_following_subquery_for_record(self, user):
-        return Relationship.objects.filter(relationship_type=FOLLOW_TYPE, from_user=user.id, to_user=OuterRef("author_id"))
+    def get_following_subquery_for_record(self, user: CustomUser) -> models.QuerySet:
+        """특정 유저가 팔로잉하는 유저 서브쿼리"""
+        return Relationship.objects.get_following_subquery_for_record(user)
