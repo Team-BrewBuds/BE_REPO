577f7fbcc81ce3f46e9e4eefc388f3117edd8651
hwstar-1204 <hwstar1204@gmail.com>
2025-07-27 17:04:25 +0900

    style : serializer doc string 추가

--- repo/interactions/relationship/serializers.py
+++ repo/interactions/relationship/serializers.py
@@ -4,11 +4,15 @@ from repo.profiles.serializers import UserSimpleSerializer
 
 
 class UserFollowListSerializer(serializers.Serializer):
+    """팔로잉/팔로워 리스트 조회 시리얼라이저"""
+
     user = UserSimpleSerializer()
     is_following = serializers.BooleanField()
 
 
 class UserBlockListSerializer(serializers.Serializer):
+    """차단 리스트 조회 시리얼라이저"""
+
     user = UserSimpleSerializer(source="to_user")
 
     def to_representation(self, instance):
